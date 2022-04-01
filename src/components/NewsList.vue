2513d25817322c33b1fecc8ab6bcff647b97a152
Matthew-Jordon Whynter <matthewjordon.whynter@mymona.uwi.edu>
2022-03-31 21:55:28 -0500

    added newslist component

---
+++ src/components/NewsList.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="news_list">
+        <li class="news_item">News</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+}
+</script>
