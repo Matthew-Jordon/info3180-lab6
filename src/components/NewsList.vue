c3c013933b452b307f39a9e5ea080ca3b272ffff
Matthew-Jordon Whynter <matthewjordon.whynter@mymona.uwi.edu>
2022-04-01 09:32:15 -0500

    created news cards

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,13 +1,58 @@
 <template>
-    <ul class="news_list">
-        <li class="news_item">News</li>
-    </ul>
+    <div class="container-fluid" id="cont">
+        <div class="card" v-for="article in articles">
+            <img :src="article.urlToImage" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="h4 card-title">{{article.title}}</p>
+                <p class="card-text">{{article.description}}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+            articles: []
+        }
     },
+    created() {
+        let self = this
+
+        fetch('https://newsapi.org/v2/top-headlines?country=us', 
+        {
+            headers: {
+                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+            }
+        }).then((response)=> {
+            return response.json()
+        
+        }).then((data)=> {
+            console.log(data)
+            self.articles = data.articles
+        })
+        }
 }
 </script>
+
+<style scoped>
+.container-fluid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.card {
+    max-width: 25%;
+    margin: .5%;
+    max-height: 10%;
+    border-bottom: greenyellow inset;
+}
+
+.card-img-top {
+    width: 100%;
+    height: auto;
+}
+
+</style>
